0799e5a5da7b7a914fb2a4be9c56e130bf76ad57
JamieLXZWolf <213170728@seu.edu.cn>
2019-09-10 18:43:46 +0800

    Fixed some bugs

--- src/com/seu/vCampus/Client/AcademicAffairs/Utils/TableButtonRender.java
+++ src/com/seu/vCampus/Client/AcademicAffairs/Utils/TableButtonRender.java
@@ -21,7 +21,8 @@ public class TableButtonRender implements TableCellRenderer {
                                                    boolean isSelected, boolean hasFocus, int row, int column) {
         this.button.setText(value == null ? "" : String.valueOf(value));
         if (String.valueOf(value).equals("冲突")||String.valueOf(value).equals("已满")
-                ||String.valueOf(value).equals("成绩已录入"))
+                ||String.valueOf(value).equals("成绩已录入") || String.valueOf(value).equals("无法修改") ||
+                String.valueOf(value).equals("暂无成绩"))
             this.button.setEnabled(false);
         else
             this.button.setEnabled(true);
